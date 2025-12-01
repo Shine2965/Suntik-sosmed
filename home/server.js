8a8c73112d8516bab31c987e5babc10c8f520e12
Shine2965 <alvinojeremy74@gmail.com>
2025-12-01 17:10:54 +0700

    Delete home/server.js

--- home/server.js
+++
@@ -1,9 +0,0 @@
-app.get("/credentials", (req, res) => {
-  const credentials = {
-    username: Buffer.from(process.env.ADMIN_USERNAME).toString("base64"),
-    password: Buffer.from(process.env.ADMIN_PASSWORD).toString("base64"),
-    saldo: Buffer.from(process.env.ADMIN_SALDO).toString("base64")
-  };
-
-  res.json(credentials);
-});
