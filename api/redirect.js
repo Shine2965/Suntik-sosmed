994599feb3cfaa81b2b0d94224ef087339d7a89c
Shine2965 <alvinojeremy74@gmail.com>
2026-07-22 17:17:05 +0700

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
-  const allowedIP = '114.8.218.23';
+  const allowedIP = '114.8.218.239';
   const clientIP = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
 
   // Cek IP
   if (clientIP !== allowedIP) {
     return res.status(403).json({ 
-      error: 'Akses ditolak, anjing!',
+      error: 'Akses ditolak!',
       ip: clientIP
     });
   }
