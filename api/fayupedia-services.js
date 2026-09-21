ecc53670adb0ab2bd5909596f779cc1b2b9795ec
Shine2965 <alvinojeremy74@gmail.com>
2026-09-21 18:04:47 +0700

    Update fayupedia-services.js

--- api/fayupedia-services.js
+++ api/fayupedia-services.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
   try {
     const apiId = parseInt(process.env.FAYUPEDIA_API_ID) || 5522;
-    const apiKey = process.env.FAYUPEDIA_API_KEY || 'fbztdy-mv2vyz-eubpwc-mobrnb-4bqift';
+    const apiKey = process.env.FAYUPEDIA_API_KEY || 'qzysdo-hzbsqd-iz9ljh-8iozzn-rucfid';
 
     if (!apiKey) {
       return res.status(500).json({
