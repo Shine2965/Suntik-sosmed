be7c139d4f45d7201f4feb8b8a9e00bb88f2de5b
Shine2965 <alvinojeremy74@gmail.com>
2026-09-21 17:59:14 +0700

    Update qris-config.js

--- api/qris-config.js
+++ api/qris-config.js
@@ -20,15 +20,15 @@ export default function handler(req, res) {
 
     try {
         // ===== QIOSPAY CONFIG =====
-        const qiospayApiKey = process.env.QIOSPAY_API_KEY || '3b44218e0fddd55b592fc57fa2773ae18982478f568eefcf1cd3f4104132fad8';
+        const qiospayApiKey = process.env.QIOSPAY_API_KEY || '';
         const merchantCode = process.env.QIOSPAY_MERCHANT_CODE || 'QP052692';
 
         // ===== INDOSMM CONFIG =====
-        const indoApiKey = process.env.INDO_API_KEY || 'f02e52d385961e16764fb7ff248487e7';
+        const indoApiKey = process.env.INDO_API_KEY || 'a1174c530b97e1bc0a7eec7baff3ac6e';
 
         // ===== ORDER SOSMED CONFIG (LEGACY) =====
         const orderApiId = parseInt(process.env.ORDER_API_ID || process.env.ORDERSOSMED_API_ID) || 11313;
-        const orderApiKey = process.env.ORDER_API_KEY || process.env.ORDERSOSMED_API_KEY || '';
+        const orderApiKey = process.env.ORDER_API_KEY || process.env.ORDERSOSMED_API_KEY || '23941803d5391da4e45a1bf4ebca52064fa17a53574d1c3655a0173dd7530fb1';
         const orderSecretKey = process.env.ORDER_SECRET_KEY || process.env.ORDERSOSMED_SECRET_KEY || 'Alvino11';
 
         // ===== FAYUPEDIA CONFIG (TETAP ADA) =====
